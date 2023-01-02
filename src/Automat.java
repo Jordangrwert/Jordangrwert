f6f215e3cbc36049ac00fc3e1b4367a08f7e0ea5
jgraewert <jgraewert@DELT276924.CMN.de>
2023-01-02 08:50:49 +0100

    Local Repository verbunden

--- src/Automat.java
+++ src/Automat.java
@@ -3,7 +3,7 @@ public class Automat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				//
+				
 		String name = "Automat AVR Q2021_FAB_A";
 		final byte PRUEFNR = 4;
 		double prozent = 53.76;
