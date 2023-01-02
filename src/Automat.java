0ef2aa6a5d9b3a0cf2edbbde8934d48e754fb777
jgraewert <jgraewert@DELT276924.CMN.de>
2023-01-02 08:39:54 +0100

    Test

---
+++ src/Automat.java
@@ -0,0 +1,19 @@
+
+public class Automat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				//
+		String name = "Automat AVR Q2021_FAB_A";
+		final byte PRUEFNR = 4;
+		double prozent = 53.76;
+		boolean status = true;
+		int euro = 142;
+		char sprachModul = 'd';
+		
+		
+		
+	
+	}
+
+}
